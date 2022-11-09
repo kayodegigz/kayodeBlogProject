da17dbdede702c3ee041b4bed010251cc34f80f3
kayodegigz <kayogunrinde@gmail.com>
2022-11-09 13:07:07 +0100

    starter files

---
+++ src/main/java/com/isw/kayodeproject/KayodeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.isw.kayodeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KayodeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KayodeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isw/kayodeproject/KayodeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isw.kayodeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KayodeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
